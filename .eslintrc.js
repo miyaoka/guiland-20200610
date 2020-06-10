7763a80635612ccea4715a97300ed2779885fc9d
Masaya Kazama <miyan@t-p.jp>
2020-06-10 22:00:16 +0900

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+const prodOnlyError = process.env.NODE_ENV === 'production' ? 'error' : 'warn'
+
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-console': prodOnlyError,
+    'no-debugger': prodOnlyError,
+    '@typescript-eslint/no-unused-vars': [
+      prodOnlyError,
+      { varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
+    ],
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+}
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <HelloWorld />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default Vue.extend({
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+})
+</script>
